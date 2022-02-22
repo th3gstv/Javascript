bb8989794b10f5c3d31229ad9b4bcff3381cce51
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-02-22 17:01:44 -0300

    loop code

---
+++ 7.1-loop.js
@@ -0,0 +1,8 @@
+for(let q = 1; q <= 30; q++){
+  let event = 'Go to the gym without cry';
+  this.recept(q, event);
+}
+
+function recept(q, event){
+  console.log('Event: ' +event+ ' of day ' +q)
+}
