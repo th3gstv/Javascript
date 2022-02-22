717ab563c5ee893937acd949f1c7d993030a4f71
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-02-22 14:43:49 -0300

    array & switch

---
+++ 5.1-array&obj.js
@@ -0,0 +1,20 @@
+// Exemplo de Array
+let allProducts = [["Rice & Beans", "Soup", "Macaroni"], 
+["Grape Juice", "Strawberry Juice", "Orange Juice"]
+];
+//console.log(allProducts);
+
+//Exemplo de Objetos
+let product = {brand: "Nike", type: "Shoes", price: "$100"};
+//console.log(product);
+//console.log(product['brand']);
+//console.log(product.price);
+//console.log(product.type);
+
+//Exemplo de Array de Objetos
+let everything = [
+  {name: 'Nike Shirt', size: 'M'},
+  {name: 'V-Lone Shirt', size: 'G'},
+  {name: 'Adidas Shirt', size: 'P'}
+]
+//console.log(everything[0].name);
---
+++ 6.1-switch.js
@@ -0,0 +1,20 @@
+let country = '';
+
+//Forma de usar vários if
+switch(country){
+  case 'Odegard':
+    alert('You pass it!')
+  break;
+
+  case 'Steve':
+    alert('Flunk out!')
+  break;
+
+  case 'Martin':
+    alert('Flunk out!')
+  break;
+
+  default:
+    alert('Say another name!')
+  break;
+}
